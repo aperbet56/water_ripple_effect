c53b5f444ba54468abe9088fcdffb1563f3c3402
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-04-21 17:19:09 +0200

    Ajout du fichier script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Initialisation du pluggin jQuery Ripples
+// Modification du comportement et de l'apparence en l'initialisant avec des options
+$(".full__landing__image").ripples({
+  resolution: 256,
+  perturbance: 0.01,
+});
